4f59044d2a6c100605708b00ab539b709f99c28b
Lenar <lenarsultanow86@gmail.com>
2023-06-08 18:13:01 +0300

    4

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Введите количество элементов в массиве строк: ");
 int size = Convert.ToInt32(Console.ReadLine());
 string[] arr1 = new string[size];
+
 for (int i = 0; i < size; i++)
 {
     Console.Write("Введите элемент массива: ");
@@ -11,3 +12,18 @@ Console.WriteLine();
 Console.Write("Введенный массив: [");
 Console.Write(string.Join(",", arr1));
 Console.Write("]");
+
+int count = 0;
+int max = 3;
+
+
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= max)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+
+
