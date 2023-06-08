8bd1a8cda40cee1981a73db356648d2ff6a9f049
Lenar <lenarsultanow86@gmail.com>
2023-06-08 18:10:03 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿Console.WriteLine("Введите количество элементов в массиве строк: ");
 int size = Convert.ToInt32(Console.ReadLine());
+string[] arr1 = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    string result = Console.ReadLine()!;
+    arr1[i] = result;
+}
+Console.WriteLine();
